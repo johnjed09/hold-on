a80c340ba48d63dfcaf91f465eedde150f8c9983
Jed Dev <getesjohnjed09@gmail.com>
2026-02-28 23:54:54 +0800

    ✨ Live timer
    Implemented live time that can manually start/stop through sending message.

---
+++ src/hold-on/hold-on.gateway.ts
@@ -0,0 +1,38 @@
+import {
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+import { HoldOnService } from './hold-on.service';
+
+@WebSocketGateway()
+export class HoldOnGateway implements OnGatewayInit {
+  @WebSocketServer()
+  server: Server;
+
+  constructor(private readonly holdOnService: HoldOnService) {}
+
+  afterInit() {
+    this.holdOnService.time$.subscribe((currentTime) => {
+      this.server.emit('timer', currentTime);
+    });
+
+    this.holdOnService.timerStop$.subscribe((isStop) => {
+      if (isStop) {
+        this.server.emit('stop', 'Timer stopped!');
+      }
+    });
+  }
+
+  @SubscribeMessage('start')
+  handleStart() {
+    this.holdOnService.start();
+  }
+
+  @SubscribeMessage('stop')
+  handleStop() {
+    this.holdOnService.stop();
+  }
+}
---
+++ src/hold-on/hold-on.service.ts
@@ -0,0 +1,41 @@
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable()
+export class HoldOnService implements OnModuleDestroy {
+  private intervalCount = 0;
+  private timer: NodeJS.Timeout | null = null;
+  private readonly oneSecond = 1000;
+
+  // This is our "Radio Station" that broadcasts the time. Add new emitters
+  private timeSource = new BehaviorSubject<number>(0);
+  public time$ = this.timeSource.asObservable();
+  private timerStopSource = new BehaviorSubject<boolean>(false);
+  public timerStop$ = this.timerStopSource.asObservable();
+
+  start() {
+    if (this.timer) return; // Prevent multiple timers
+    this.tick();
+  }
+
+  private tick() {
+    this.intervalCount++;
+    this.timeSource.next(this.intervalCount); // Broadcast to listeners
+
+    console.log('1 second passed.', this.intervalCount);
+
+    this.timer = setTimeout(() => this.tick(), this.oneSecond);
+  }
+
+  stop() {
+    if (this.timer) {
+      clearTimeout(this.timer);
+      this.timer = null;
+      this.timerStopSource.next(true);
+    }
+  }
+
+  onModuleDestroy() {
+    this.stop();
+  }
+}
